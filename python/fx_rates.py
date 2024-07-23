18c11b97019dd40d70b6d930269e57fbb824ba8b
Antonio M <amoralesch@hotmail.com>
2024-07-22 22:30:21 -0400

    Extract connection to own file

--- python/fx_rates.py
+++ python/fx_rates.py
@@ -1,80 +1,8 @@
 from decimal import Decimal
 import subprocess
-import psycopg2
-from psycopg2.extras import execute_values
-from configparser import ConfigParser
+from utils.connection import Connection
 
-BASE_DIR = './secrets/database.ini'
 MAIN_LEDGER = "../ledger/hledger.all.journal"
-DB_SCHEMA = 'hledger'
-
-
-def load_config(filename=BASE_DIR, section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-    config = {}
-
-    if parser.has_section(section):
-        params = parser.items(section)
-
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return config
-
-
-def connect(config, schema: str):
-    """ Connect to the PostgreSQL database server """
-    try:
-        with psycopg2.connect(
-                options=f'-c search_path={schema}',
-                **config) as conn:
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-
-
-def create_connection(schema: str):
-    return connect(load_config(), schema)
-
-
-class Connection:
-    def __init__(self, schema: str = DB_SCHEMA):
-        self.conn = create_connection(schema)
-        self.cursor = self.conn.cursor()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.conn.commit()
-
-    def delete_table(self, table: str) -> None:
-        sql = f'truncate table {table}'
-        self.cursor.execute(sql)
-
-    def bulk_insert(self, table, fields, params):
-        sql = (
-            f'insert into {table} '
-            f'({", ".join(fields)}) '
-            f'values '
-            f'%s')
-
-        execute_values(self.cursor, sql, params)
-
-    def add_fx_rates(self, fx_rates: dict[str, dict[tuple[str, str], Decimal]]):
-        table = 'fx_rate'
-        fields = ('date', 'rate', 'currency', 'target_currency')
-        params = [
-            (timestamp, rate, original_other[0], original_other[1])
-            for timestamp, records in fx_rates.items()
-            for original_other, rate in records.items()
-        ]
-
-        self.delete_table(table)
-        self.bulk_insert(table, fields, params)
 
 
 def hledger_command(args):
