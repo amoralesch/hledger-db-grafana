8813b92b20745ac08dd7ce9ef4c768868e511d4d
Antonio M <amoralesch@hotmail.com>
2024-07-25 14:35:38 -0400

    Add views for yearly averages

---
+++ postgres/3-views.sql
@@ -0,0 +1,82 @@
+create view hledger.average_daily_expenses as
+select
+    a.date,
+    b.target_currency as currency,
+    (
+        sum(a.balance * b.rate) -
+        LAG(sum(a.balance * b.rate), 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+    ) /
+    (
+        EXTRACT(epoch FROM
+            a.date -
+            LAG(a.date, 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+        ) / 86400
+    ) as daily_expense
+from hledger.balance_to_date a
+join hledger.fx_rate b on
+    a.date = b.date and
+    a.currency = b.currency
+join hledger.main_commodities c on
+    b.target_currency = c.currency
+where
+    a.account = 'Expenses'
+group by
+    a.date,
+    a.account,
+    b.target_currency;
+
+create view hledger.average_daily_income as
+select
+    a.date,
+    b.target_currency as currency,
+    (
+        sum(a.balance * b.rate) -
+        LAG(sum(a.balance * b.rate), 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+    ) /
+    (
+        EXTRACT(epoch FROM
+            a.date -
+            LAG(a.date, 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+        ) / 86400
+    ) as daily_income
+from hledger.balance_to_date a
+join hledger.fx_rate b on
+    a.date = b.date and
+    a.currency = b.currency
+join hledger.main_commodities c on
+    b.target_currency = c.currency
+where
+    a.account = 'Income'
+group by
+    a.date,
+    a.account,
+    b.target_currency;
+
+create view hledger.average_daily_savings as
+select
+    a.date,
+    b.target_currency as currency,
+    (
+        sum(a.balance * b.rate) -
+        LAG(sum(a.balance * b.rate), 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+    ) /
+    (
+        EXTRACT(epoch FROM
+            a.date -
+            LAG(a.date, 365) OVER (PARTITION BY a.account, b.target_currency ORDER BY a.date)
+        ) / 86400
+    ) as daily_savings
+from hledger.balance_to_date a
+join hledger.fx_rate b on
+    a.date = b.date and
+    a.currency = b.currency
+join hledger.main_commodities c on
+    b.target_currency = c.currency
+join hledger.assets_classification d on
+    a.account = d.account
+where
+    d.classification = 'Savings'
+group by
+    a.date,
+    a.account,
+    b.target_currency;
