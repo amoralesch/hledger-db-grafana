3fed3123aeed40d04561ec52a2178a273e065d0c
Antonio M <amoralesch@hotmail.com>
2024-08-04 19:27:24 -0400

    Remove postgresql from the mix

--- postgres/4-data.sql
+++
@@ -1,9 +0,0 @@
-COPY hledger.assets_classification
-FROM '/docker-entrypoint-initdb.d/csv/assets_classification.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY hledger.main_commodities
-FROM '/docker-entrypoint-initdb.d/csv/main_commodities.csv'
-DELIMITER ','
-CSV HEADER;
