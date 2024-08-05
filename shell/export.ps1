29cd24a2b4171ac6929b46f8ae3e8ded8933df81
Antonio M <amoralesch@hotmail.com>
2024-08-05 18:03:13 -0400

    Add new -d flag to script

--- shell/export.ps1
+++ shell/export.ps1
@@ -3,19 +3,22 @@ Set-StrictMode -Version Latest
 
 $ledgerFile = ''
 $startDate = ''
+$depthLevel = ''
 
 function Print-Usage {
-    Write-Host "Usage: export.ps1 -f <ledger_file> -b <start_date>"
+    Write-Host "Usage: export.ps1 -f <ledger_file> -b <start_date> -d <depth>"
     exit 1
 }
 
 param(
     [string]$f,
-    [string]$b
+    [string]$b,
+    [string]$d
 )
 
 if ($f) { $ledgerFile = $f }
 if ($b) { $startDate = $b }
+if ($d) { $depthLevel = $d }
 
 # Confirm python3 is installed
 python3 --version
@@ -25,7 +28,7 @@ python3 -m venv venv
 & "$venvPath\Scripts\Activate.ps1"
 pip install -r "./python/requirements.txt"
 
-python3 "./python/full_import.py" -f $ledgerFile -b $startDate
+python3 "./python/full_import.py" -f $ledgerFile -b $startDate -d $depthLevel
 
 # all is good, deactivate virtual environment
 deactivate
