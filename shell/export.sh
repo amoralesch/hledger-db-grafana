00c94c18fdc656aceaf85cebd9d83a73266ff277
Antonio M <amoralesch@hotmail.com>
2024-08-05 09:10:27 -0400

    Accept and pass -d flag in shell script

--- shell/export.sh
+++ shell/export.sh
@@ -3,11 +3,13 @@ set -e
 
 ledger_file=''
 start_date=''
+depth_level=''
 
-while getopts 'f:b:' flag; do
+while getopts 'f:b:d:' flag; do
   case "${flag}" in
     f) ledger_file="${OPTARG}" ;;
     b) start_date="${OPTARG}" ;;
+    d) depth_level="${OPTARG}" ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -21,7 +23,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r ./python/requirements.txt
 
-python3 ./python/full_import.py -f ${ledger_file} -b ${start_date}
+python3 ./python/full_import.py -f ${ledger_file} -b ${start_date} -d ${depth_level}
 
 # all is good, deactivate virtual environment
 deactivate
